a5208c5766422e9284540277c36178bb5a8b240f
ICHIKAWA, Yuji <ichikawa.yuji@gmail.com>
2018-08-11 17:48:22 +0900

    experiment 3 of workaround for modal on ios

--- docs/js/az.js
+++ docs/js/az.js
@@ -1,13 +1,35 @@
+/* "passive" が使えるかどうかを検出 */
+let passiveSupported = false;
+try {
+    document.addEventListener('test', null, Object.defineProperty(
+        {},
+        'passive',
+        {
+            get: function() {
+                passiveSupported = true;
+            }
+        }
+    ));
+} catch(err) {}
+
 function preventDefault(e) {
     e.preventDefault();
 }
 
 $(document.body).on('show.bs.modal', '.modal', function() {
-    document.body.addEventListener("touchmove", preventDefault, { passive: false });
+    document.body.addEventListener(
+        'touchmove',
+        preventDefault,
+        passiveSupported ? { passive: false } : false
+    );
 });
 
 $(document.body).on('hide.bs.modal', '.modal', function() {
-    document.body.removeEventListener("touchmove", preventDefault, { passive: false });
+    document.body.removeEventListener(
+        'touchmove',
+        preventDefault,
+        passiveSupported ? { passive: false } : false
+    );
 });
 
 /**
