7860415928cf02ecbad1a082f7ee5ffd0f5924a3
ICHIKAWA, Yuji <ichikawa.yuji@gmail.com>
2018-08-12 22:47:07 +0900

    disable ios_workaround

--- docs/js/az.js
+++ docs/js/az.js
@@ -1,37 +1,3 @@
-/* "passive" が使えるかどうかを検出 */
-let passiveSupported = false;
-try {
-    document.addEventListener('test', null, Object.defineProperty(
-        {},
-        'passive',
-        {
-            get: function() {
-                passiveSupported = true;
-            }
-        }
-    ));
-} catch(err) {}
-
-function preventDefault(e) {
-    e.preventDefault();
-}
-
-$(document.body).on('show.bs.modal', '.modal', function() {
-    document.body.addEventListener(
-        'touchmove',
-        preventDefault,
-        passiveSupported ? { passive: false } : false
-    );
-});
-
-$(document.body).on('hide.bs.modal', '.modal', function() {
-    document.body.removeEventListener(
-        'touchmove',
-        preventDefault,
-        passiveSupported ? { passive: false } : false
-    );
-});
-
 /**
  * @file 音声合成のラッパー関数群です。
  */
