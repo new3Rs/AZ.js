dca64d8097eb4649a41e14d55b2dcd07cefed190
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2019-10-02 19:16:33 +0900

    npm audit fix

--- docs/js/az-worker.js
+++ docs/js/az-worker.js
@@ -97,7 +97,7 @@
    * http://mongodb.github.io/node-mongodb-native/api-bson-generated/objectid.html#objectid-isvalid
    */
   ObjectID.isValid = function(objectid) {
-    if(!objectid) return false;
+    if(!objectid || (typeof objectid !== 'string' && (typeof objectid !== 'object' || typeof objectid.toString !== 'function'))) return false;
 
     //call .toString() to get the hex if we're
     // working with an instance of ObjectID
@@ -213,13 +213,15 @@
     return out;
   }
 
+  var inspect = (Symbol && Symbol.for('nodejs.util.inspect.custom')) || 'inspect';
+
   /**
    * Converts to a string representation of this Id.
    *
    * @return {String} return the 24 byte hex string representation.
    * @api private
    */
-  ObjectID.prototype.inspect = function() { return "ObjectID("+this+")" };
+  ObjectID.prototype[inspect] = function() { return "ObjectID("+this+")" };
   ObjectID.prototype.toJSON = ObjectID.prototype.toHexString;
   ObjectID.prototype.toString = ObjectID.prototype.toHexString;
 
